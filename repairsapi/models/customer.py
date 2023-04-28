41f7415caf4e4fba21401b7f755d920ed786d779
Freya Potempa <FreyaPotempa@gmail.com>
2023-04-28 09:13:20 -0500

    first commit

---
+++ repairsapi/models/customer.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+
+    # Relationship to the built-in User model which has name and email
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Additional address field to capture from the client
+    address = models.CharField(max_length=155)
+
+    @property
+    def full_name(self):
+        '''defines full name'''
+        if hasattr(self.user, 'first_name') and hasattr(self.user, 'last_name'):
+            return f'{self.user.first_name} {self.user.last_name}'
