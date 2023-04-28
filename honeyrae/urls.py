d738ed68d27c5e377284edafb80a442a7a104611
Freya Potempa <FreyaPotempa@gmail.com>
2023-04-28 13:29:22 -0500

    chapter 5

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from repairsapi.views import register_user, login_user
+from rest_framework import routers
+from repairsapi.views import CustomerView, EmployeeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
+router.register(r'employees', EmployeeView, 'employee')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
